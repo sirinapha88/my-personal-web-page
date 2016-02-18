cbb083ac2b14172056f895da056c749b631eecb6
Edward Tierra <edrichtierra@gmail.com>
2016-02-17 20:41:04 -0800

    create some more views and add app routes for angular in app.js

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+var app = angular.module('myPersonalWebsite', ['ngRoute']);
+app.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: "./views/home.html",
+        controller: "HomeController"
+    }).when('/contact', {
+        templateUrl: "./views/contact.html",
+        controller: "ContactController"
+    }).when('/projects', {
+        templateUrl: "./views/projects.html",
+        controller: "ProjectsController"
+    }).when('/resume', {
+        templateUrl: "./views/resume.html",
+        controller: "ResumeController"
+    }).otherwise({
+        redirectTo: "/"
+    });
+});
