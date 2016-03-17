3a0439c6be6833b8bf6af6a3e906ea63f581e7a0
Edward Tierra <edrichtierra@gmail.com>
2016-03-16 23:55:23 -0700

    add link directory in controllers for $http

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -5,7 +5,7 @@ app.controller("ContactController", function($scope, $location){
 
 });
 app.controller("ProjectsController", function($scope, $http, $location){
-    $http.get()
+    $http.get('../public/json/projects.json')
     .then(function(data){
         $scope.classification = data;
     });
