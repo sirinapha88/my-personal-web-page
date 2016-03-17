9d715287035f89add582ce0155b80d74cfbcfa4b
Edward Tierra <edrichtierra@gmail.com>
2016-03-16 23:49:39 -0700

    add $http into controllers.js

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -4,7 +4,7 @@ app.controller("HomeController", function($scope, $location){
 app.controller("ContactController", function($scope, $location){
 
 });
-app.controller("ProjectsController", function($scope, $location){
+app.controller("ProjectsController", function($scope, $http, $location){
 
 });
 app.controller("ResumeController", function($scope, $location){
