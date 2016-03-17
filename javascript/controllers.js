474d1539a6ba3677a52a93703728a22ce3ae5454
Edward Tierra <edrichtierra@gmail.com>
2016-03-16 23:52:18 -0700

    add http in controllers

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -5,7 +5,10 @@ app.controller("ContactController", function($scope, $location){
 
 });
 app.controller("ProjectsController", function($scope, $http, $location){
-
+    $http.get()
+    .then(function(data){
+        $scope.classification = data;
+    });
 });
 app.controller("ResumeController", function($scope, $location){
 
