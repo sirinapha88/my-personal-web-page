e72e936c1c366fc771d02c6730ab59f4671027ff
Edward Tierra <edrichtierra@gmail.com>
2016-03-17 23:32:46 -0700

    remove $http in controllers

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -4,11 +4,8 @@ app.controller("HomeController", function($scope, $location){
 app.controller("ContactController", function($scope, $location){
 
 });
-app.controller("ProjectsController", function($scope, $http, $location){
-    $http.get('../public/json/projects.json')
-    .then(function(data){
-        $scope.classification = data;
-    });
+app.controller("ProjectsController", function($scope, $location){
+    //add repeat in here
 });
 app.controller("ResumeController", function($scope, $location){
 
