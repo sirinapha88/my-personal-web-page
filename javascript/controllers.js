58aef3828517fed52f716f59a5fd4b578670a7f4
Edward Tierra <edrichtierra@gmail.com>
2016-03-19 09:49:03 -0700

    add bike parking in controllers object

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -8,22 +8,26 @@ app.controller("ProjectsController", function($scope, $location){
     //add repeat in here
     $scope.projects = [
         {
-            projectName: ,
+            projectName: "Bike Parking",
+            miniName: "in San Francisco",
+            url: "http://thatsedrich.github.io/project-app/",
+            imageURL: "http://i.imgur.com/vKjfqPlb.jpg"
+        },
+        {
+            projectName: "Fight Matching",
+            miniName: "for Kickboxing and MMA",
             url: ,
             imageURL:
         },
         {
             projectName: ,
+            miniName: ,
             url: ,
             imageURL:
         },
         {
             projectName: ,
-            url: ,
-            imageURL:
-        },
-        {
-            projectName: ,
+            miniName: ,
             url: ,
             imageURL:
         }
