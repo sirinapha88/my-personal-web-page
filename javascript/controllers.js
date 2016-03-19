d827c5d5b4fee0d83ebd371b95100f31c5dc0164
Edward Tierra <edrichtierra@gmail.com>
2016-03-18 23:57:52 -0700

    create objects within objects

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -7,24 +7,11 @@ app.controller("ContactController", function($scope, $location){
 app.controller("ProjectsController", function($scope, $location){
     //add repeat in here
     $scope.projects = {
-        projectName: ,
-        url: ,
-        imageURL:
-    },
-    $scope.projects = [{
-        projectName: ,
-        url: ,
-        imageURL:
-    },
-    $scope.projects = [{
-        projectName: ,
-        url: ,
-        imageURL:
-    },
-    $scope.projects = [{
-        projectName: ,
-        url: ,
-        imageURL:
+        {
+            projectName: ,
+            url: ,
+            imageURL:
+        }
     }
 });
 app.controller("ResumeController", function($scope, $location){
